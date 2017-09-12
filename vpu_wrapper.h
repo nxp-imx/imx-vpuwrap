d9ce1eddf72e2e7ef07f5a341374ef8d1c6d344c
Song Bing <bing.song@nxp.com>
2017-09-12 10:12:48 -0800

    Increase to version 2.1.0
    Signed-off-by: Song Bing <bing.song@nxp.com>

--- vpu_wrapper.h
+++ vpu_wrapper.h
@@ -29,7 +29,7 @@ extern "C" {
 /**************************** version info ***********************************/
 #define VPU_WRAPPER_VERSION(major, minor, release)	 \
 	(((major) << 16) + ((minor) << 8) + (release))
-#define VPU_WRAPPER_VERSION_CODE	VPU_WRAPPER_VERSION(2, 0, 0)
+#define VPU_WRAPPER_VERSION_CODE	VPU_WRAPPER_VERSION(2, 1, 0)
 
 /**************************** decoder part **********************************/
 
